51ed62a54b163f5cc7340924e5f5fc80dfe2fcd7
Nir-tuvia <nirtah@gmail.com>
2015-12-01 16:57:14 +0200

    Existing code for multiThreadedServer, still figuring out why it is starting and then closing

--- src/MainTestClass.java
+++ src/MainTestClass.java
@@ -1,4 +1,11 @@
 
 public class MainTestClass {
-	//Hi Yafim baby.
+	//!!!TEST!!! Trying to make the thread pool work.
+
+	public static void main(String[] args) {
+		MultiThreadedClass server = new MultiThreadedClass(8080);
+		server.startTheServer(server);
+
+	}
+
 }
